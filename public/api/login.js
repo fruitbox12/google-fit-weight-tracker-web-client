4d3d15f5f0800e3f5e1fbc813dfc6876c1a5cb5c
fruitbox12 <dylanwong007@gmail.com>
2023-06-08 02:36:57 -0700

    Update login.js

--- public/api/login.js
+++ public/api/login.js
@@ -10,27 +10,40 @@ module.exports = async (req, res) => {
     // Pre-flight request. Reply successfully:
     return res.status(200).end();
   }
-
-  const { userId } = req.body;
-  const secretKey = 'your_secret_key'; // Replace this with your actual secret key
+   let userId;
 
   if (req.method === 'POST') {
-    if (!userId) {
-      return res.status(400).json({ error: 'Invalid request' });
-    }
-  }
-else if (req.method === 'GET') {
-
-
-    const token = jwt.sign({ id: "example@google" }, secretKey, {
-      expiresIn: '1h', // token will expire in 1 hour
+    // Wait for the request body stream to finish and parse it as JSON
+    const body = await new Promise((resolve, reject) => {
+      let bodyChunks = [];
+      req.on('data', chunk => {
+        bodyChunks.push(chunk);
+      }).on('end', () => {
+        const rawBody = Buffer.concat(bodyChunks).toString();
+        try {
+          const parsedBody = JSON.parse(rawBody);
+          resolve(parsedBody);
+        } catch (e) {
+          reject(new Error('Invalid JSON'));
+        }
+      });
     });
-    return res.status(200).json({ token });
-  }
-    const token = jwt.sign({ id: userId }, secretKey, {
-      expiresIn: '1h', // token will expire in 1 hour
-    });
 
+    userId = body.userId;
+  } else if (req.method === 'GET') {
+    // Get userId from query parameters for GET requests
+    userId = req.query.userId;
+  }
+
+  const secretKey = 'your_secret_key'; // Replace this with your actual secret key
+
+  if (!userId) {
+    return res.status(400).json({ error: 'Invalid request' });
+  }
+
+  const token = jwt.sign({ id: userId }, secretKey, {
+    expiresIn: '1h', // token will expire in 1 hour
+  });
 
   // If we're here, then the method is not allowed.
   res.setHeader('Allow', 'POST, GET, OPTIONS');
