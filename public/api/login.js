28e1a21834553a2b4dea28b1453b06b1c8ca1e4f
fruitbox12 <dylanwong007@gmail.com>
2023-06-08 00:45:06 -0700

    Update login.js

--- public/api/login.js
+++ public/api/login.js
@@ -1,15 +1,16 @@
 // pages/api/login.js
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === 'POST' || "GET") {
     // Here we just return a simple message for simplicity.
     res.status(200).json({ message: 'You are logged in.' });
 
     // After the user logs in, we redirect them to another page where we perform the Ethereum wallet login.
     res.redirect("/eth_login");
+    
   } else {
     // Handle any other HTTP method
-    res.setHeader('Allow', ['POST']);
+    res.setHeader('Allow', ['POST','GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
